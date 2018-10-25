fdd6d8ceed65f693f1404a45bb97302604a6e228
Theo Winnett <rachwinn13@gmail.com>
2018-10-25 18:05:12 -0500

    Finished

--- main.py
+++ main.py
@@ -1,65 +1,140 @@
-from flask import Flask, request, redirect, render_template
+from flask import  Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:deucalion@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:deucalion@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y33tiyghjgkcjg'
 
-# Makes my class for the database
+# How to password check?
+
+# Makes my class for the databases
 class Post(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(120))
 	text = db.Column(db.String(120))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-	def __init__(self, title, text):
+	def __init__(self, title, text, owner):
 		self.title = title 
 		self.text = text
+		self.owner = owner
+													#classes (models)
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(120))
+	password = db.Column(db.String(120))
+	blogs = db.relationship('Post', backref='owner')
 
-# Home page you first load into, takes the forms from 'newpost' and adds to DB
+	def __init__(self, username, password):
+		self.username = username 
+		self.password = password
+
+@app.before_request
+def require_login():
+	allowed_routes = ['login', 'signup']
+	if request.endpoint not in allowed_routes and 'username' not in session:
+		flash("Login!")
+		return redirect('/login')
+	else:
+		redirect('/')
+	
+#handlers (controlers)
+
 @app.route('/', methods=['GET', 'POST'])
-def index():# for title 
+def index():
+	 
+	owner = User.query.filter_by(username=session['username']).first()
 	if request.method == 'POST':   
 		text_input = request.form['text']
 		title_input = request.form['title']
-		# DB stuff
-		title = Post(title_input,text_input)
+		# Blog stuff
+		title = Post(title_input,text_input,owner)
 		db.session.add(title)
 		db.session.commit()
-		title = Post.query.all()
-		return render_template('dashboard.html', title = title, text= title)
+		post_db = Post.query.all()
+		users = User.query.all()
+		return render_template('dashboard.html', post_db = post_db,  users = users)
 	else:
-		title = Post.query.all()
-		return render_template('dashboard.html', title = title, text = title)
- 
-#renders submit page
-@app.route('/newpost', methods=["GET", "POST"])
-def submit():
-	#attempt at error, gives key error for both 'title' and 'text'
-	#============================================================
-	#if request.method == 'POST':
-	#	title_input = request.form['title']
-	#	text_input = request.form['text']
-	#	if title_input and text_input == '':
-	#		error = 'You must submit something in BOTH fields!'
-	#		return render_template('submit-page.html', error= error)
-	#else:
-	#	error = ''
-	return render_template('submit-page.html')
-	#, error= error)
+		post_db = Post.query.all()
+		users = User.query.all()
+		return render_template('dashboard.html', post_db = post_db, users = users)
 
-#renders each blog post (Eventually)
+#renders individual blogs
+
 @app.route('/blog', methods=["GET"])
 def display():
 	individual = request.args.get('id')
+	individual_user = request.args.get('user')
+	users = User.query.all()
 	if individual:
-		thing = Post.query.get(individual)
-		return render_template('blog.html', thing = thing)
+		post_db = Post.query.filter_by(id = individual)
+		return render_template('blog.html', post_db = post_db, users = users)
+	elif individual_user:
+		post_db = Post.query.filter_by(user_id = individual_user)
+		return render_template('blog.html', post_db = post_db, users = users)
 	else:
-		all_posts = Post.query.all()
-		return render_template('dashboard.html', title = all_posts, text = all_posts )
+		post_db = Post.query.all()
+		return render_template('blog.html', post_db = post_db, users = users)
+
+#renders submit page
+
+@app.route('/newpost', methods=["GET", "POST"])
+def submit():
+	return render_template('submit-page.html')
+
+#renders signup page (NEW HANDLERS BEGIN)
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		verify = request.form['verify']
+		existing_user = User.query.filter_by(username=username).first()
+		if not existing_user:
+			username = request.form['username']
+			password = request.form['password']
+			verify = request.form['verify']
+			new_user = User(username, password)
+			db.session.add(new_user)
+			db.session.commit()
+			session['username'] = username
+			return redirect('/')
+		else:
+			flash("Duplicate User", "error")
+			return render_template('signup.html')
+
+#renders login
+
+@app.route('/login', methods= ["GET", "POST"])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+		user = User.query.filter_by(username=username).first()
+		if user and user.password == password:
+			session['username'] = username
+			flash("Logged in")
+			return redirect('/')
+		else:
+			flash("User password incorrect, or user does not exist", "error")		
+			return render_template('login.html')
+	else:
+		return render_template('login.html')
+
+#performs logout
+
+@app.route('/logout')
+def logout():
+	del session['username']
+	return redirect('/')
+
+
+
 
 
 if __name__ == '__main__':
